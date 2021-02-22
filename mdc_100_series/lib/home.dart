a8364628f81a4d807c4b1511f4d00a9d885a4a94
Rohit Bhargava <rohitbhargava586@gmail.com>
2021-02-23 01:29:26 +0530

    Card gridview added

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -55,8 +55,11 @@ class HomePage extends StatelessWidget {
         title: Text('SHRINE'),
       ),
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: <Widget>[Card()],
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
